ae0f5d56cd1c12692e4bd6888149872260686466
Michael Moore <michael_m@cimbura.com>
2016-08-05 01:10:28 -0500

    Dash WIP

--- lib/wp-geometa-dash.php
+++ lib/wp-geometa-dash.php
@@ -21,6 +21,234 @@ class WP_GeoMeta_Dash {
 	}
 
 	/**
+	 * Get the functions by type
+	 */
+	public static function get_functions_by_type() {
+
+		$cap_cats = array(
+			'geom_relationship' => array(
+				'label' => 'Spatial Relationship Tests',
+				'desc' => 'Test topological relationships between two geometries. Some functions may work on a shape\'s bounding box rather than the shape itself.',
+				'funcs' => array(
+					'Contains',
+					'Crosses',
+					'Disjoint',
+					'Distance',
+					'Equals',
+					'Intersects',
+					'MBRContains',
+					'MBRCoveredBy',
+					'MBRDisjoint',
+					'MBREqual',
+					'MBREquals',
+					'MBRIntersects',
+					'MBROverlaps',
+					'MBRTouches',
+					'MBRWithin',
+					'Overlaps',
+					'ST_Contains',
+					'ST_Crosses',
+					'ST_Dimension',
+					'ST_Disjoint',
+					'ST_Distance',
+					'ST_Equals',
+					'ST_Intersects',
+					'ST_Overlaps',
+					'ST_Touches',
+					'ST_Within',
+					'Touches',
+					'Within',
+				)
+			),
+
+			'properties' => array(
+				'label' => 'Geometry Properties',
+				'desc' => 'These functions analyize spatial properties of a single geometry.',
+				'funcs' => array(
+					'Area',
+					'GLength',
+					'IsClosed',
+					'IsEmpty',
+					'IsRing',
+					'IsSimple',
+					'SRID',
+					'ST_Area',
+					'ST_GeometryType',
+					'ST_IsClosed',
+					'ST_IsEmpty',
+					'ST_IsRing',
+					'ST_IsSimple',
+					'ST_IsValid',
+					'ST_Length',
+					'ST_SRID',
+					'ST_Validate',
+				)
+			),
+
+			'investigation' => array(
+				'label' => 'Geometry Disection',
+				'desc' => 'Investigate the type and sub-parts of a geometry.',
+				'funcs' => array(
+					'Dimension',
+					'EndPoint',
+					'ExteriorRing',
+					'GeometryN',
+					'GeometryType',
+					'InteriorRingN',
+					'NumGeometries',
+					'NumInteriorRings',
+					'NumPoints',
+					'PointN',
+					'ST_EndPoint',
+					'ST_ExteriorRing',
+					'ST_GeometryN',
+					'ST_InteriorRingN',
+					'ST_NumGeometries',
+					'ST_NumInteriorRings',
+					'ST_NumPoints',
+					'ST_PointN',
+					'ST_StartPoint',
+					'ST_X',
+					'ST_Y',
+					'StartPoint',
+					'X',
+					'Y',
+				)
+			),
+
+			'make_new_geom' => array(
+				'label' => 'Generate new Geometry',
+				'desc' => 'Create a new geometry based on existing geometries and spatial operations.',
+				'funcs' => array(
+					'Boundary',
+					'Buffer',
+					'Centroid',
+					'ConvexHull',
+					'Envelope',
+					'ST_Boundary',
+					'ST_Buffer',
+					'ST_Centroid',
+					'ST_ConvexHull',
+					'ST_Difference',
+					'ST_Envelope',
+					'ST_Intersection',
+					'ST_Simplify',
+					'ST_SymDifference',
+					'ST_Union',
+				)
+			),
+
+			'change_format' => array(
+				'label' => 'Data Format Helpers',
+				'desc' => 'Create or convert geometries from various input and output formats.',
+				'funcs' => array(
+					'GeomCollFromText',
+					'GeomCollFromWKB',
+					'GeometryCollection',
+					'GeometryCollectionFromText',
+					'GeometryCollectionFromWKB',
+					'GeometryFromText',
+					'GeometryFromWKB',
+					'GeomFromText',
+					'GeomFromWKB',
+					'LineFromText',
+					'LineFromWKB',
+					'LineString',
+					'LineStringFromText',
+					'LineStringFromWKB',
+					'MLineFromText',
+					'MLineFromWKB',
+					'MPointFromText',
+					'MPointFromWKB',
+					'MPolyFromText',
+					'MPolyFromWKB',
+					'MultiLineString',
+					'MultiLineStringFromText',
+					'MultiLineStringFromWKB',
+					'MultiPoint',
+					'MultiPointFromText',
+					'MultiPointFromWKB',
+					'MultiPolygon',
+					'MultiPolygonFromText',
+					'MultiPolygonFromWKB',
+					'Point',
+					'PointFromText',
+					'PointFromWKB',
+					'PolyFromText',
+					'PolyFromWKB',
+					'Polygon',
+					'PolygonFromText',
+					'PolygonFromWKB',
+					'ST_GeomCollFromText',
+					'ST_GeomCollFromWKB',
+					'ST_GeometryCollectionFromText',
+					'ST_GeometryCollectionFromWKB',
+					'ST_GeometryFromText',
+					'ST_GeometryFromWKB',
+					'ST_GeomFromGeoJSON',
+					'ST_GeomFromText',
+					'ST_GeomFromWKB',
+					'ST_LineFromText',
+					'ST_LineFromWKB',
+					'ST_LineStringFromText',
+					'ST_LineStringFromWKB',
+					'ST_PointFromGeoHash',
+					'ST_PointFromText',
+					'ST_PointFromWKB',
+					'ST_PolyFromText',
+					'ST_PolyFromWKB',
+					'ST_PolygonFromText',
+					'ST_PolygonFromWKB',
+					'AsBinary',
+					'AsText',
+					'AsWKB',
+					'AsWKT',
+					'ST_AsBinary',
+					'ST_AsGeoJSON',
+					'ST_AsText',
+					'ST_AsWKB',
+					'ST_AsWKT',
+				)
+			),
+			'other' => array(
+				'label' => 'Miscellaneous Functions',
+				'desc' => 'Other little-used functions.',
+				'funcs' => array(
+					'PointOnSurface',
+					'ST_Buffer_Strategy',
+					'ST_Distance_Sphere',
+					'ST_GeoHash',
+					'ST_LatFromGeoHash',
+					'ST_LongFromGeoHash',
+					'ST_PointOnSurface',
+					'ST_Relate',
+				),
+			),
+		);
+
+		$our_caps = WP_GeoUtil::get_capabilities();
+
+		$our_cap_cats = array();
+
+		foreach ( $cap_cats as $category => $funcinfo ) {
+
+			sort( $funcinfo['funcs'] );
+			foreach	( $funcinfo['funcs'] as $func ) {
+				if ( in_array( strtolower( $func ), $our_caps ) ) {
+					$our_cap_cats[ $category ]['funcs'][] = $func;
+				}
+			}
+
+			if ( !empty( $our_cap_cats[ $category ] ) ) {
+				$our_cap_cats[ $category ][ 'label' ] = $funcinfo[ 'label' ];
+				$our_cap_cats[ $category ][ 'desc' ] = $funcinfo[ 'desc' ];
+			}
+		}
+
+		return $our_cap_cats;
+	}
+
+	/**
 	 * Set up our filters
 	 */
 	protected function __construct() {
